85e9a28f4ed6c9555b71726d2ec3a9a219a5db6f
adbenitez <asieldbenitez@gmail.com>
2021-03-20 17:15:46 -0400

    enable read receipts by default

--- src/simplebot/bot.py
+++ src/simplebot/bot.py
@@ -283,7 +283,6 @@ class DeltaBot:
             # set some useful bot defaults on the account
             delete_server_after=1,
             delete_device_after=2592000,
-            mdns_enabled=0,
             save_mime_headers=1,
             e2ee_enabled=1,
             sentbox_watch=0,
