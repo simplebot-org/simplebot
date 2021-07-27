7b1edc9b4f892fad3643e01b0c735a925af3360e
adbenitez <asieldbenitez@gmail.com>
2021-07-26 23:05:15 -0400

    illustrate in the examples how to use the new users preferences

--- examples/impersonating.py
+++ examples/impersonating.py
@@ -1,6 +1,9 @@
 """
-This example illustrates how to send a message impersonating another user
-in groups.
+This example illustrates:
+
+* how to send a message impersonating another user.
+* how to define preferences that can be customized by each user.
+
 This plugin uses a 3rd party lib so you will need to install it first:
 
     $ pip install wikiquote
@@ -12,20 +15,20 @@ import wikiquote
 import simplebot
 
 
+@simplebot.hookimpl
+def deltabot_init(bot):
+    bot.add_preference("locale", "Bot language, example values: en, es, de")
+
+
 @simplebot.command
 def quote(message, replies):
     """Get quote of the day from Wikiquote."""
-    text, author = wikiquote.quote_of_the_day()
-    if message.chat.is_group():
-        replies.add(text=text, sender=author)
-    else:
-        replies.add(text='"{}"\n\n― {}'.format(text, author))
+    lang = bot.get("locale", scope=message.get_sender_contact().addr)
+    text, author = wikiquote.quote_of_the_day(lang=lang)
+    replies.add(text=text, sender=author)
 
 
 class TestImpersonating:
     def test_impersonating(self, mocker):
         msg = mocker.get_one_reply(text="/quote")
-        assert not msg.override_sender_name
-
-        msg = mocker.get_one_reply(text="/quote", group="mockgroup")
         assert msg.override_sender_name
