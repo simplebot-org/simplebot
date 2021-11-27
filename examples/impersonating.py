82c7c5e32dba1b24edf570aba88e06dcf6bf5ddf
adbenitez <asieldbenitez@gmail.com>
2021-11-26 20:51:07 -0500

    tweak examples/impersonating.py

--- examples/impersonating.py
+++ examples/impersonating.py
@@ -1,37 +1,28 @@
 """
 This example illustrates:
 
-* how to send a message impersonating another user.
+* how to send a message impersonating another username.
 * how to define preferences that can be customized by each user.
-
-This plugin uses a 3rd party lib so you will need to install it first:
-
-    $ pip install wikiquote
-
 """
-
-import wikiquote
 
 import simplebot
 
 
 @simplebot.hookimpl
 def deltabot_init(bot):
-    bot.add_preference("locale", "Bot language, example values: en, es, de")
+    bot.add_preference("name", "the name to impersonate")
 
 
-@simplebot.command
-def quote(bot, message, replies):
-    """Get quote of the day from Wikiquote."""
-    lang = bot.get("locale", scope=message.get_sender_contact().addr)
-    if lang:
-        text, author = wikiquote.quote_of_the_day(lang=lang)
-    else:
-        text, author = wikiquote.quote_of_the_day()
-    replies.add(text=text, sender=author)
+@simplebot.filter
+def filter_messages(bot, message, replies):
+    """Send me any message in private and I will reply with the same message but impersonating another username."""
+    if not message.chat.is_group() and message.text:
+        sender = message.get_sender_contact()
+        name = bot.get("name", scope=sender.addr) or sender.name
+        replies.add(text=message.text, sender=name)
 
 
 class TestImpersonating:
     def test_impersonating(self, mocker):
-        msg = mocker.get_one_reply(text="/quote")
+        msg = mocker.get_one_reply(text="hello")
         assert msg.override_sender_name
